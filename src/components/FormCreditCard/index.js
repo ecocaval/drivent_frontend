f01800ed8f97d1c59df1646b06faa9a56475b0b6
Juan Sued <juansued19@gmail.com>
2023-05-10 23:28:33 -0300

    feature/implementando formulário de cartão de crédio

---
+++ src/components/FormCreditCard/index.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Cards from 'react-credit-cards';
+import 'react-credit-cards/es/styles-compiled.css';
+import styled from 'styled-components';
+
+const FormCreditCard = ({ formData, setFormData }) => {
+  const { cvc, expiry, name, number } = formData;
+  const [focusedField, setFocusedField] = useState('');
+
+  const inputRefs = {
+    number: useRef(null),
+    name: useRef(null),
+    expiry: useRef(null),
+    cvc: useRef(null),
+  };
+
+  const handleInputFocus = (fieldName) => {
+    setFocusedField(fieldName);
+  };
+
+  useEffect(() => {
+    if (focusedField && inputRefs[focusedField].current) {
+      inputRefs[focusedField].current.focus();
+    }
+  }, [focusedField, inputRefs]);
+
+  const onInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+  return (
+    <FormCreditCardStyle id="PaymentForm">
+      <div className="cardContainer">
+        <Cards cvc={cvc} expiry={expiry} focused={focusedField} name={name} number={number} />
+      </div>
+
+      <form>
+        <input
+          ref={inputRefs.number}
+          type="tel"
+          name="number"
+          placeholder="Card Number"
+          onChange={onInputChange}
+          onFocus={() => handleInputFocus('number')}
+          maxLength={16}
+          min={0}
+        />
+
+        <input
+          ref={inputRefs.name}
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={onInputChange}
+          onFocus={() => handleInputFocus('name')}
+          maxLength={15}
+          min={0}
+        />
+
+        <div className="containerDoble">
+          <input
+            ref={inputRefs.expiry}
+            type="tel"
+            name="expiry"
+            placeholder="Valid Thru"
+            onChange={onInputChange}
+            onFocus={() => handleInputFocus('expiry')}
+            maxLength={4}
+            min={0}
+          />
+          <input
+            ref={inputRefs.cvc}
+            type="tel"
+            name="cvc"
+            placeholder="CVC"
+            onChange={onInputChange}
+            onFocus={() => handleInputFocus('cvc')}
+            maxLength={3}
+            min={0}
+          />
+        </div>
+      </form>
+    </FormCreditCardStyle>
+  );
+};
+
+export default FormCreditCard;
+
+const FormCreditCardStyle = styled.div`
+  display: flex;
+
+  .cardContainer {
+    display: grid;
+    place-items: center;
+  }
+
+  form {
+    padding: 10px 10px 10px 20px;
+    display: grid;
+    gap: 10px;
+    max-width: 600px;
+    .containerDoble {
+      display: flex;
+      gap: 10px;
+      max-width: 100%;
+    }
+  }
+`;
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,19 @@
+import { useState } from 'react';
+import FormCreditCard from '../../../components/FormCreditCard';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const [formData, setFormData] = useState({
+    cvc: '',
+    expiry: '',
+    name: '',
+    number: '',
+  });
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // Faça algo com os dados do formulário completo, incluindo o PaymentForm
+    console.log(formData);
+  };
+
+  return <FormCreditCard formData={formData} setFormData={setFormData} />;
 }
