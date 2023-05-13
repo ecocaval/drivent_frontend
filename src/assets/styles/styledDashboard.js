b6f4a7d9ae36722761ac202aa829514983c6505b
Ottoniel Hapuc <dossantoshapuc@gmail.com>
2023-05-13 18:13:19 -0300

    feat: click on payment functionalities, presentation of assignments according to the user's progress

--- src/assets/styles/styledDashboard.js
+++ src/assets/styles/styledDashboard.js
@@ -5,8 +5,17 @@ export const AreaTitle = styled.h1`
   margin-bottom: 37px;
 `;
 
-export const AreaSubTitle = styled.h3` 
+export const AreaSubTitle = styled.h3`
   font-size: 1.2rem;
-  margin: ${props => props.margin || '0'};
+  margin: ${(props) => props.margin || '0'};
   color: var(--font-gray);
+`;
+
+export const ProceedButton = styled.button`
+  width: 182px;
+  height: 37px;
+  background: var(--button-proceed);
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  border: none;
 `;
---
+++ src/components/Ticket/cardTicket/index.js.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+export const CardTicket = ({ e, selectedTicket, setSelectedTicket }) => {
+  return (
+    <>
+      <CardTickets onClick={() => setSelectedTicket(e)} selectedTicket={selectedTicket} e={e}>
+        {e}
+      </CardTickets>
+    </>
+  );
+};
+export const CardTickets = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 145px;
+  height: 145px;
+  background-color: ${(props) => (props.selectedTicket === props.e ? 'var(--color-ticketcardx2)' : 'var(while)')};
+  border: 1px solid var(--color-ticketcard);
+  border-radius: 20px;
+  margin: 0 24px 44px 0;
+`;
