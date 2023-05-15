5a887b48373f819ccdd50f7b51e797ad3d0fef73
Ottoniel Hapuc <dossantoshapuc@gmail.com>
2023-05-15 15:30:00 -0300

    feat: cardx2 and refactoring bug cards

--- src/components/Ticket/cardTicket/index.js.js
+++ src/components/Ticket/cardTicket/index.js.js
@@ -40,3 +40,28 @@ export const CardTickets = styled.div`
     margin: 5px;
   }
 `;
+
+export const CardTicketsx2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 290px;
+  height: 145px;
+  background-color: var(--card-default-grayx2);
+  border: 1px solid var(--card-default-gray);
+  border-radius: 20px;
+  margin: 0 24px 44px 0;
+
+  font-size: 1em;
+  :hover{
+    cursor: pointer;
+  }
+
+  >p{
+    display: inline-block;
+    color: var(--font-gray);
+    font-size: 0.8em;
+    margin: 5px;
+  }
+`;
