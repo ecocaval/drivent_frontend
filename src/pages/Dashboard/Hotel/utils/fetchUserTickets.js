3fb60578521fedcbb31c2f4f8890b72dfefbf1d0
Ottoniel Hapuc <dossantoshapuc@gmail.com>
2023-05-16 12:39:20 -0300

    refactory: ajustment previus commit

--- src/pages/Dashboard/Hotel/utils/fetchUserTickets.js
+++ src/pages/Dashboard/Hotel/utils/fetchUserTickets.js
@@ -15,7 +15,6 @@ export default async function fetchUserTickets(token, setUserTicketIncludesHotel
 
     const [ticketType] = ticketTypes.filter((ticket) => ticket.id === userTickets.ticketTypeId);
 
-
     if (!ticketType.includesHotel) {
       setUserTicketIncludesHotel(false);
     }
